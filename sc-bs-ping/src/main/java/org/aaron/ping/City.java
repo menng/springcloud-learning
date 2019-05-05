92c122d0457457326afaf8f18e7d406cb8452f06
Menng <concisesky@gmail.com>
2019-05-05 20:00:43 +0800

    ping->pong

--- sc-bs-ping/src/main/java/org/aaron/ping/City.java
+++ sc-bs-ping/src/main/java/org/aaron/ping/City.java
@@ -4,16 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
-/**
- * TODO
- * @author mengxy
- * @create 2018/11/20 20:03
- */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,14 +18,31 @@ import java.util.Arrays;
 public class City {
     private Integer id;
     private String name;
+    private String description;
 }
 
 @RestController
+@RequestMapping("/ping")
 class CityController {
+
+    @Autowired
+    private PongCityFeign pongCityFeign;
 
     @RequestMapping("/city/list")
     public Object listCity() {
-        return Arrays.asList(City.builder().id(1).name("北京").build()
-                , City.builder().id(2).name("上海").build());
+        return Arrays.asList(City.builder().id(1).name("北京").description("ping").build()
+                , City.builder().id(2).name("上海").description("ping").build());
     }
+
+    @RequestMapping("/pong/city/list")
+    public Object listPongCity() {
+        return pongCityFeign.listPongCity();
+    }
+}
+
+@FeignClient(name = "sc-bs-pong")
+interface PongCityFeign {
+
+    @RequestMapping("/pong/city/list")
+    Object listPongCity();
 }
